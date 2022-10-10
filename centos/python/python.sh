8c2655058fec6d7737fc972fa45f64c3e8f10b26
chiyonn <chiyonn@localhost.localdomain>
2022-10-10 19:35:25 +0900

    bug fix

--- centos/python/python.sh
+++ centos/python/python.sh
@@ -1,6 +1,11 @@
 sudo yum groupinstall "Development Tools" -y
-sudo yum install openssl-devel libffi-devel bzip2-devel -y
+sudo yum install epel-release openssl11 openssl11-devel libffi-devel bzip2-devel -y
 sudo wget https://www.python.org/ftp/python/3.10.6/Python-3.10.6.tgz
+
+export CFLAGS=$(pkg-config --cflags openssl11)
+export LDFLAGS=$(pkg-config --libs openssl11)
+echo "export CFLAGS=$(pkg-config --cflags openssl11)" >> $HOME/.bashrc
+echo "export LDFLAGS=$(pkg-config --libs openssl11)" >> $HOME/.bashrc
 
 tar xvf Python-3.10.6.tgz
 cd Python-3.10.6
