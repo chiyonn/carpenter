4fb4707eee3bd307c59bd5fccf5dc8de5562321e
chiyonn <chiyonn@localhost.localdomain>
2022-10-10 19:18:25 +0900

    added alias for python and pip

--- centos/python/python.sh
+++ centos/python/python.sh
@@ -9,18 +9,7 @@ sudo make altinstall
 
 cd ..
 sudo rm -fr Python-3.10.6
-sudo rm Python3.10.6.tgz
+sudo rm Python-3.10.6.tgz
 
-if [ -f $HOME/.bash_aliases ]
-then
-    grep "alias python=" $HOME/.bash_aliases
-    if [ $0 = 1 ] then
-        echo "alias python='python3.10'" >> $HOME/.bash_aliases
-    fi
-    grep "alias pip=" $HOME/.bash_aliases
-    if [ $0 = 1 ] then
-        echo "alias pip='pip3.10'" >> $HOME/.bash_aliases
-    fi
-else
-    WORNING=$WORNING"FROM PYTHON: You don't have .bash_aliases at home directory, failed to add aliases 'python' and 'pip'\n"
-fi
+echo "alias python='python3.10'" >> $HOME/.bashrc
+echo "alias pip='pip3.10'" >> $HOME/.bashrc
